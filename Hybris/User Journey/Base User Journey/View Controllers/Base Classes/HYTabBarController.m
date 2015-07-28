58db27337315d8b50f939916c872b0219e2e46a0
debidutt_home <debidutt.nist@gmail.com>
2015-07-29 03:47:30 +0530

    TabController made working for the time being
    Doubt in triggering product search ?

--- Hybris/User Journey/Base User Journey/View Controllers/Base Classes/HYTabBarController.m
+++ Hybris/User Journey/Base User Journey/View Controllers/Base Classes/HYTabBarController.m
@@ -23,11 +23,11 @@
     NSLog(@"I am in HYTabBarController Screen");
     
     //Set the tabbar strings   
-    [[self.viewControllers objectAtIndex:0] setTitle:NSLocalizedStringWithDefaultValue(@"Shop", nil, [NSBundle mainBundle], @"Shop", @"Home tab title")];
+    [[self.viewControllers objectAtIndex:0] setTitle:NSLocalizedStringWithDefaultValue(@"Shop", nil, [NSBundle mainBundle], @"Home", @"Home tab title")];
     [[self.viewControllers objectAtIndex:1] setTitle:NSLocalizedStringWithDefaultValue(@"Account", nil, [NSBundle mainBundle], @"Account", @"Account tab title")];;
     [[self.viewControllers objectAtIndex:2] setTitle:NSLocalizedStringWithDefaultValue(@"Scan", nil, [NSBundle mainBundle], @"Scan", @"Scan tab title")];
     [[self.viewControllers objectAtIndex:3] setTitle:NSLocalizedStringWithDefaultValue(@"Stores", nil, [NSBundle mainBundle], @"Stores", @"Store Locator tab title")];
-    [[self.viewControllers objectAtIndex:4] setTitle:NSLocalizedStringWithDefaultValue(@"Basket", nil, [NSBundle mainBundle], @"Basket", @"Basket tab title")];
+//    [[self.viewControllers objectAtIndex:4] setTitle:NSLocalizedStringWithDefaultValue(@"Basket", nil, [NSBundle mainBundle], @"Basket", @"Basket tab title")];
 }
 
 
